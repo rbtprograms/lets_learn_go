5a89f2f41ff2dc074c0d7808e02ab5c4f2649e90
Robert Thompson <rbtprograms@gmail.com>
2019-06-09 21:25:08 -0700

    more going on!

--- section_3/main.go
+++ section_3/main.go
@@ -6,9 +6,18 @@ func main() {
 	fmt.Println("Hello World")
 	testFunction()
 
+	n, _ := fmt.Println("Howdy", 42, true)
+	fmt.Println(n)
+
 	for i := 1; i <= 10; i++ {
 		fmt.Println(i)
 	}
+	x := 42
+	fmt.Println(x);
+	x = 99
+	fmt.Println(x)
+	var y = 101
+	fmt.Println(y)
 }
 
 func testFunction() {
