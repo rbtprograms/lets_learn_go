dc21b80f4cf59d0b18bfbcf5ce0cbce385f59cdf
Robert Thompson <rbtprograms@gmail.com>
2019-06-13 22:20:10 -0700

    exercise 1 done

---
+++ section_10/main.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+func main() {
+	exercise1()
+	// exercise2()
+	// exercise3()
+	// exercise4()
+	// exercise5()
+	// exercise6()
+	// exercise7()
+	// exercise8()
+	// exercise9()
+}
+
+func exercise1() {
+	a := [5]int{1,2,3,4,5}
+	for i, v := range a {
+		fmt.Println(i, v)
+	}
+	fmt.Printf("%T ", a)
+}
+
+func exercise2() {
+	for i := 65; i <= 90; i++ {
+		for j := 0; j <= 2; j++ {
+			fmt.Printf("%v\t%#U\n", i, i)
+		}
+	}
+}
+
+func exercise3() {
+	bd := 1990
+	for bd < 2020 {
+		fmt.Println(bd)
+		bd++
+	}
+}
+
+func exercise4() {
+	bd := 1990
+	for {
+		if bd == 2020 {
+			break
+		}
+		fmt.Println(bd)
+		bd++
+	}
+}
+
+func exercise5() {
+	for i := 10; i <= 100; i++ {
+		fmt.Printf("%v\n", i%4)
+	}
+}
+
+var temp = 5
+
+func exercise6() {
+	if temp < 6 {
+		fmt.Print("worked")
+	}
+}
+func exercise7() {
+	if temp > 6 {
+		fmt.Print("worked")
+	} else {
+		fmt.Print("didnt")
+	}
+}
+func exercise8() {
+	switch {
+	case false:
+		fmt.Println("should not print")
+	case true:
+		fmt.Println("Will print")
+	}
+}
+func exercise9() {
+	favSport := "surfing"
+	switch favSport {
+	case "skiing":
+		fmt.Println("skiing")
+	case "surfing":
+		fmt.Println("Hell yea brother")
+	}
+}
