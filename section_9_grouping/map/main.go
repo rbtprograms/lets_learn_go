df7be80a18c514f5053dd175f5ab86d02300630f
Robert Thompson <rbtprograms@gmail.com>
2019-06-13 22:08:06 -0700

    maps are like objects

--- section_9_grouping/map/main.go
+++ section_9_grouping/map/main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 )
 
-//This is kind of like how objects in JS point to the same object,
-//so copying them is tricky. Slices will point to the same underlying
-//array, so if one of your slices changes the underlying array it will
-//affect another slice using data from that same array.
+//maps are kinda like objects, fast lookup, no order,
+//key value pairing
 func main() {
-	x := []int{65,43,6,88,32,56,9,32,4,67}
-	fmt.Println(x)
-	y := append(x[0:2], x[5:]...) //new underlying array stores the value of new slice
+	m := map[string]int{
+		"Bobby":29,
+		"Sophia":27,
+	}
+	fmt.Println(m)
+	fmt.Println(m["Sophia"])
+	
+	//the comma ok idiom
+	if v, ok := m["Bobby"]; ok {
+		fmt.Println("THE IF PRINT: ", v)
+	}
 
-	fmt.Println(x)
-	fmt.Println(y)
+	if v, ok := m["Injoong"]; ok {
+		fmt.Println("SECOND IF PRINT: ", v)
+	}
 }
