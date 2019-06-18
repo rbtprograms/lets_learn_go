bba75f0b6530e0ae511f543ab6d2cc3145579086
Robert Thompson <rbtprograms@gmail.com>
2019-06-18 07:03:42 -0700

    variadic spread is about all that was

---
+++ section_13_functions/spread/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	test()
+	xi := []int{2,3,4,5,6,7,8,9}
+	res := test2(xi...)
+	fmt.Println(res)
+}
+
+func test() {
+	fmt.Println("TEST")
+}
+
+func test2(x ...int) int {
+					 //^^^this makes a slice
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	sum := 0
+	for _, v := range x {
+		sum += v
+		fmt.Println("item: ", v, " and sum: ", sum)
+	}
+	return sum
+}
