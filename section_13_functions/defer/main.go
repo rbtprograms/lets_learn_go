bb33cb94b2708990d32d045c8306be26368a3667
Robert Thompson <rbtprograms@gmail.com>
2019-06-18 07:13:23 -0700

    methods are kinda cool

--- section_13_functions/defer/main.go
+++ section_13_functions/defer/main.go
@@ -3,27 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	test()
-	xi := []int{2,3,4,5,6,7,8,9}
-	res := test2(xi...)
-	fmt.Println(res)
+	//runs second, at end of containing function
+	defer test()
+	test2()
 }
 
 func test() {
 	fmt.Println("TEST")
 }
 
-func test2(x ...int) int {
-					 //^^^this makes a slice
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
-	fmt.Println(len(x))
-	fmt.Println(cap(x))
-
-	sum := 0
-	for _, v := range x {
-		sum += v
-		fmt.Println("item: ", v, " and sum: ", sum)
-	}
-	return sum
+func test2() {
+	fmt.Println("SECOND TEST")
 }
---
+++ section_13_functions/methods/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type been struct {
+	person
+	been bool
+}
+
+func (s been) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+
+func main() {
+	b1 := been{
+		person: person{
+			first: "Bouby",
+			last:  "Bubbero",
+			age:   29,
+		},
+		been: true,
+	}
+	b2 := been{
+		person: person{
+			first: "Subby",
+			last:  "Subbero",
+			age:   27,
+		},
+		been: true,
+	}
+	//embedded fields are promoted up but can still be 
+	//accessed via their type
+	fmt.Println(b1)
+	fmt.Println(b2)
+	b1.speak()
+	b2.speak()
+}
